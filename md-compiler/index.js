f64fbdf1a997f7b0a2e3ffa5a5f6551f3a411912
trainingmontage <nemo.omen@gmail.com>
2021-10-02 06:11:49 -0500

    Fail with regex

---
+++ md-compiler/index.js
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import path from 'path';
+import { readFileSync } from 'fs';
+import ReadLine from 'readline';
+
+
+const __dirname = path.resolve();
+
+const args = process.argv.slice(2);
+const filePath = path.join(__dirname, args[0]);
+
+const fileContents = readFileSync(filePath).toString();
+
+const ri = ReadLine.createInterface({
+  input: fs.createReadStream(filePath),
+  output: process.stdout,
+  console: false,
+});
+
+ri.on('line', function(line) {
+  splitLines(line);
+});
+
+function splitLines(line) {
+  const tokens = line.split('');
+  const tokanVals = tokens.map((token) => {
+    return {
+      type: getTokenType(token),
+      value: token,
+      leftSib: tokens[tokens.indexOf(token) - 1],
+      rightSib: tokens[tokens.indexOf(token) + 1],
+    }
+  });
+  console.log(tokanVals);
+}
+
+function getTokenType(token) {
+  let tokenType = undefined;
+  // console.log(token);
+  console.log(/^[A-Za-z]&/.test(token));
+  return tokenType;
+}
