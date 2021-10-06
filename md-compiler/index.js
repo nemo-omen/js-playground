e83967219e56cd6e405e07b69385982ecfde8d95
trainingmontage <nemo.omen@gmail.com>
2021-10-05 21:32:35 -0500

    Parse?

--- md-compiler/index.js
+++ md-compiler/index.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { readFileSync } from 'fs';
 import ReadLine from 'readline';
 import { Scanner } from './Scanner.js';
+import { Console } from 'console';
 
 const __dirname = path.resolve();
 
@@ -11,8 +12,37 @@ const filePath = path.join(__dirname, args[0]);
 
 const fileContents = readFileSync(filePath).toString();
 
-const scanner = new Scanner(fileContents);
+const rl = ReadLine.createInterface({
+  input: fs.createReadStream(filePath),
+  // output: process.stdout,
+});
 
-scanner.scanTokens();
+rl.on('line', (line) => {
+  parseCharGroups(line);
+});
 
-console.log(scanner.getTokens());
+function parseCharGroups(line) {
+  const groupArray = line.split(' ');
+  let lexedLine = [];
+  for(let group of groupArray) {
+    const lex = assignLexeme(group);
+    lexedLine.push(lex);
+  }
+  console.log(lexedLine);
+}
+
+function assignLexeme(charGroup) {
+  if(/#{5}/g.test(charGroup)) return {lex: 'H5', val: charGroup};
+  if(/#{4}/g.test(charGroup)) return {lex: 'H4', val: charGroup};
+  if(/#{3}/g.test(charGroup)) return {lex: 'H3', val: charGroup};
+  if(/#{2}/g.test(charGroup)) return {lex: 'H2', val: charGroup};
+  if(/#{1}/g.test(charGroup)) return {lex: 'H1', val: charGroup};
+  if(charGroup === '') return {lex: 'E', val: charGroup};
+  return {lex: 'WORD', val: charGroup};
+}
+
+// const scanner = new Scanner(fileContents);
+
+// scanner.scanTokens();
+
+// console.log(scanner.getTokens());
