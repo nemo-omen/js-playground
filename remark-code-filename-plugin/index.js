a5e4114e7768ba34277a97b3ec4650b54c589aa0
nemo-omen <nemo.omen@gmail.com>
2021-10-08 08:29:39 -0500

    Continue to fail at mdast

--- remark-code-filename-plugin/index.js
+++ remark-code-filename-plugin/index.js
@@ -3,10 +3,7 @@ import fs from 'fs';
 import { readFileSync } from 'fs';
 import { unified } from 'unified';
 import { remark } from 'remark';
-import remarkParse from 'remark-parse';
-import remark2rehype from 'remark-rehype';
-import rehypeStringify from 'rehype-stringify';
-import { reporter } from 'vfile-reporter';
+import html from 'remark-html';
 import codeFilename from './remark-code-filename.js';
 
 const __dirname = path.resolve();
@@ -15,13 +12,13 @@ const args = process.argv.slice(2);
 const filePath = path.join(__dirname, args[0]);
 const buffer = fs.readFileSync(filePath);
 
-const contents = unified()
-  .use(remarkParse)
-  .use(remark2rehype)
+
+function mdToHTML(buffer) {
+  return remark()
   .use(codeFilename)
-  .use(rehypeStringify)
-  .process(buffer)
-  .then((file) => {
-    console.log(String(file));
-    console.error(reporter(file));
-  });
+  .use(html)
+  .processSync(buffer)
+  .toString();
+}
+
+console.log(mdToHTML(buffer));
--- remark-code-filename-plugin/remark-code-filename.js
+++ remark-code-filename-plugin/remark-code-filename.js
@@ -1,20 +1,39 @@
 import { visit } from 'unist-util-visit';
-import { toString } from 'mdast-util-to-string';
 
 export default function remarkCodeFilename() {
   return (tree, file) => {
-    // console.log(tree);
     visit(
       tree,
-      // (node) => node.tagName === 'code',
-      (node) => node.tagName === 'pre' && node.children.some(n => n.tagName === 'code'),
-      (node, meta) => {
-        // if (!meta) return;
+      'code',
+      (codeNode) => {
+        const fileName = codeNode.meta;
 
-        // const fileName = meta.value.trim();
-        // console.log((fileName));
-        node.tagName = 'div';
+        if(!fileName) return;
+
+        const code = {...codeNode};
+
+        const captionElement = {
+          type: 'element',
+          data: {hName: 'figcaption'},
+          children: [{type: 'text', value: fileName}]
+        };
+
+        const figureElement = {
+          type: 'element',
+          data: {hName: 'figure'},
+          children: [captionElement, code]
+        };
+
+        replace(codeNode, figureElement);
       }
-    );
+    )
   }
+}
+
+function replace(source, target) {
+  for(const property in source) {
+    delete source[property];
+  }
+
+  Object.assign(source, target);
 }
